ef22dccf0cbc66a1386cc2c026bf48c1d3d0d655
hack874 <g0217261@gmail.com>
2022-12-20 16:43:48 +0900

    ブログ投稿一覧表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,8 +7,10 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-public function index(Post $post)
-{
-    return $post->get();
+    // public function index(Post $post)
+    public function index() //関数を定義している
+    {
+        $post = Post::get(); //modelのにある"post"を持ってきて、$postに入れる。そしてviewに渡す
+        return view('posts/index')->with(['posts' => $post]);  //viewにお願いしている
+    } //view('posts/index')=viewのPost.indexというファイルを指定してる
 }
-    }
